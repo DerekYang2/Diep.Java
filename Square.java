4cdc123103cfa9916fd4932bf5aa325691c10bb2
DerekYang2 <derekyang243@gmail.com>
2024-03-10 09:38:13 -0400

    Bouncing Square

--- Square.java
+++ Square.java
@@ -1,50 +1,63 @@
 import java.awt.*;
-// Add back updatadable 
-public class Square implements Drawable{
-  protected double x, y, vx, vy;
-  protected int id;
-  Stopwatch stopwatch;
-  public Square() {
-      createId();  // Remember to create an id on creation
-      addToPools();  // Remember to add object to pool on creation
 
-      stopwatch = new Stopwatch();
-      stopwatch.start();
+// Add back updatadable
+public class Square implements Drawable, Updatable {
+    protected double x, y, vx, vy;
+    protected int id;
+    Stopwatch stopwatch;
 
-      x = Math.random() * Main.windowWidth;
-      y = Math.random() * Main.windowHeight;
-  }
+    public Square() {
+        createId();  // Remember to create an id on creation
+        addToPools();  // Remember to add object to pool on creation
 
-  public void update() {
-      
-  }
+        stopwatch = new Stopwatch();
+        stopwatch.start();
 
-  public void draw(Graphics g) {
-    int sqaureLength = 10;
-    int sqaureWidth = 10;
-    // this is not good probably
-    g.setColor(Color.BLUE);
-    g.fillRect((int)x, (int)y, sqaureWidth, sqaureLength);
-  }
+        x = Math.random() * Main.windowWidth;
+        y = Math.random() * Main.windowHeight;
+        vx = Math.random() * 5;
+        vy = Math.random() * 5;
+    }
 
-  // Deletable Methods
-  public void createId() {
-      this.id = Main.idServer.getId();
-  }
+    public void update() {
+        x += vx;
+        y += vy;
+        // Bounce off walls
+        if (x < 0 || x > Main.windowWidth) {
+            vx *= -1;
+        }
+        if (y < 0 || y > Main.windowHeight) {
+            vy *= -1;
+        }
+    }
 
-  public int getId() {
-      return this.id;
-  }
+    public void draw(Graphics g) {
+        int squareLength = 10;
+        int squareWidth = 10;
+        // this is not good probably
+        g.setColor(Color.BLUE);
+        g.fillRect((int) x, (int) y, squareWidth, squareLength);
+    }
 
-  public void addToPools() {
-      Main.drawablePool.addObj(this);
-  }
+    // Deletable Methods
+    public void createId() {
+        this.id = Main.idServer.getId();
+    }
 
-  public void delete() {
-      // All added to wait lists
-      Main.drawablePool.deleteObj(this.getId());
-      Main.updatablePool.deleteObj(this.getId());
-      Main.idServer.returnId(this.getId());
-  }
-  
+    public int getId() {
+        return this.id;
+    }
+
+    public void addToPools() {
+        Main.drawablePool.addObj(this);
+        Main.updatablePool.addObj(this);
+    }
+
+    public void delete() {
+        // All added to wait lists
+        Main.drawablePool.deleteObj(this.getId());
+        Main.updatablePool.deleteObj(this.getId());
+        Main.idServer.returnId(this.getId());
+    }
+
 }

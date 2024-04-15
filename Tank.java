6a2f8b218d71e2e0a1c14339e52bf1b9224ec223
Derek Yang <derekyang243@gmail.com>
2024-04-15 10:19:35 -0400

    Libraries included

--- Tank.java
+++ Tank.java
@@ -35,7 +35,7 @@ public class Tank extends GameObject {
         this.controller.setHost(this);
 
         scale = (float)Math.pow(1.01, (level - 1));
-        setTankBuild(TankBuild.createTankBuild("penta shot"));
+        setTankBuild(TankBuild.createTankBuild("annihilator"));
     }
 
     public void setTankBuild(TankBuild tankBuild) {
